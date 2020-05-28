eca09fa92b65650d40fcf016785e3c85e3fdd8e5
felixmariotto <46470486+felixmariotto@users.noreply.github.com>
2020-05-28 11:15:12 +0200

    add bundling to package commands

--- webpack.config.js
+++ webpack.config.js
@@ -1,26 +1,42 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
-module.exports = {
+module.exports = env => {
 
-  entry: path.resolve(__dirname, 'src'),
+  let mode = "development";
+  let devtool = 'eval-source-map';
 
-  output: {
-    path: path.resolve(__dirname, 'dist'),
-    filename: 'bundle.js'
-  }, 
+  // Prod environment
+  if (env.NODE_ENV === 'prod') {
+    devtool = false;
+    mode = 'production';
+    // outputPath = `${__dirname}/build/js`;
+  };
 
-  devServer: {
-    contentBase: path.resolve(__dirname, 'dist'),
-  },
+  return {
 
-  plugins: [
-    new HtmlWebpackPlugin({
-      title: "three.js VR starter app",
-      template: path.resolve(__dirname, 'src/template.html')
-    })
-  ],
+    mode: mode,
 
-  devtool: 'eval-source-map'
+    entry: path.resolve(__dirname, 'src'),
 
-}
+    output: {
+      path: path.resolve(__dirname, 'dist'),
+      filename: 'bundle.js'
+    }, 
+
+    devServer: {
+      contentBase: path.resolve(__dirname, 'dist'),
+    },
+
+    plugins: [
+      new HtmlWebpackPlugin({
+        title: "three.js VR starter app",
+        template: path.resolve(__dirname, 'src/template.html')
+      })
+    ],
+
+    devtool: devtool
+
+  }
+
+};
